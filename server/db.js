25162456c83134d5874ae83e068dcad366b0024a
unknown <luisvasquezg116@gmail.com>
2025-08-14 13:43:25 -0500

    cambio2

--- server/db.js
+++ server/db.js
@@ -1,15 +1,18 @@
-const mysql = require('mysql');
+const { Client } = require('pg');
 
-const db = mysql.createConnection({
-  host: 'dpg-d2f0mc49c44c739gpgc0-a',
+const db = new Client({
+  host: 'dpg-d2f0mc49c44c739gpgc0-a.oregon-postgres.render.com', // host completo de Render
+  port: 5432,
   user: 'pendientes_qpqm_user',
   password: 'egqTxo1SdPbsnoiTRQ8NM9SvQZgNWeEd',
-  database: 'pendientes_qpqm'
+  database: 'pendientes_qpqm',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('Conectado a MySQL');
-});
+db.connect()
+  .then(() => console.log('Conectado a PostgreSQL'))
+  .catch(err => console.error('Error de conexión', err.stack));
 
 module.exports = db;
